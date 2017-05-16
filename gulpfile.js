7d37c21da0797f639b5115d20f9ea09029b3580e
liding <895905300@qq.com>
2017-05-16 19:19:28 +0800

    Madman

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,12 @@ var gulp = require('gulp');
 
 var sass = require('gulp-sass');
 
-gulp.task('sass',function () {
-    gulp.src('src/sass/*.scss')
+gulp.task('all',function () {
+    gulp.src('src/sass/*.scss') //对sass编译
         .pipe(sass())
-        .pipe(gulp.dest('dist/css'))
+        .pipe(gulp.dest('dist/css'));
+    gulp.src('bower_components/jquery/dist/jquery.min.js')
+        .pipe(gulp.dest('dist/js'));
+    gulp.src('src/js/*.js')
+        .pipe(gulp.dest('dist/js'));
 });
--- src/js/madman.js
+++ src/js/madman.js
@@ -3,7 +3,37 @@
  */
 const md = {
     version : '0.1',
-    dialog:function () {
-        
+    alertDialog:function () {
+        //console.log($event)
+        $('body').append(`<div class="md-dialog">
+           <div class="dialog-title">
+                测试头
+           </div>
+           <div class="dialog-content">
+               内容
+           </div>
+           <div class="dialog-button">
+                <span>确定</span>
+           </div>
+        </div>`);
+        this.shadow();
+        this.dialogResize();
+        return function () {
+            
+        }
+    },
+    dialogResize:function () {
+        let dialog = $('.md-dialog');
+        let width = $(window).width();
+        let height = $(window).height();
+        let dialogLeft = (width - dialog.width()) / 2;
+        let dialogTop = (height - dialog.height()) / 2;
+        dialog.css({'left': dialogLeft, 'top': dialogTop,'display': 'block','transform':'scale(1)','opacity':1});
+    },
+    shadow:function () {
+        $('body').append(`<div class="md-shadow"></div>`)
     }
 };
+$(window).resize(function() {
+    md.dialogResize();
+});
